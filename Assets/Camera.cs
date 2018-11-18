67c27395e8e84b7b7a672896736a30d27d202c2c
ImmanuelBaskaran <immanuel.baskaran.16@ucl.ac.uk>
2018-11-18 00:03:35 +0000

    Intial

---
+++ Assets/Camera.cs
@@ -0,0 +1,22 @@
+﻿// This script lets you enable/disable fog per camera.
+// by enabling or disabling the script in the title of the inspector
+// you can turn fog on or off per camera.
+
+using UnityEngine;
+using System.Collections;
+
+public class ExampleClass : MonoBehaviour
+{
+    private bool revertFogState = false;
+
+    void OnPreRender()
+    {
+        revertFogState = RenderSettings.fog;
+        RenderSettings.fog = enabled;
+    }
+
+    void OnPostRender()
+    {
+        RenderSettings.fog = revertFogState;
+    }
+}
---
+++ Assets/scripts/ShotEffectsManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class ShotEffectsManager : MonoBehaviour
+{
+    [SerializeField] ParticleSystem muzzleFlash;
+    [SerializeField] AudioSource gunAudio;
+    [SerializeField] GameObject impactPrefab;
+
+    ParticleSystem impactEffect;
+
+    //Create the impact effect for our shots
+    public void Initialize()
+    {
+        impactEffect = Instantiate(impactPrefab).GetComponent<ParticleSystem>();
+    }
+
+    //Play muzzle flash and audio
+    public void PlayShotEffects()
+    {
+        muzzleFlash.time = 0;
+        muzzleFlash.Play(true);
+    }
+
+    //Play impact effect and target position
+    public void PlayImpactEffect(Vector3 impactPosition)
+    {
+        impactEffect.transform.position = impactPosition;
+        impactEffect.time = 0;
+        impactEffect.Play();
+        gunAudio.Stop();
+        gunAudio.Play();
+    }
+}
